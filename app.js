406bccf392d348ee793874d5e5d7092a07b51b7b
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-07-19 10:20:24 +0200

    delete get methods at app.js (refactor)

--- app.js
+++ app.js
@@ -12,36 +12,4 @@ app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
 hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
-
-app.get('/beers', (req, res, next) => {
-  punkAPI.getBeers()
-    .then(beers => {
-      res.render('beers', { beers });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
-
-app.get('/random-beer', (req, res, next) => {
-  const randomNum = Math.floor(Math.random() * 25);
-  console.log(randomNum);
-  punkAPI.getBeers()
-    .then(beers => {
-      console.log('aquí va la random beer', beers[randomNum]);
-      const randomBeer = beers[randomNum];
-      res.render('random-beers', randomBeer);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
-
-app.get('/*', (req, res, next) => {
-  res.render('page-not-found');
-});
-
 app.listen(3000);
